4b8f8a90dc86a78dfc2c2152c6f7ef6f011e0fbe
Kyle <mustangcoupe69@gmail.com>
2014-08-15 21:32:05 -0600

    Updated to latest

--- Java/src/org/magnos/steer/behavior/SteerSeparation.java
+++ Java/src/org/magnos/steer/behavior/SteerSeparation.java
@@ -16,58 +16,58 @@ public class SteerSeparation<V extends Vec<V>> extends AbstractSteerSpatial<V>
 
     protected V towards;
     protected V force;
-	
-	public SteerSeparation(SpatialDatabase<V> space, float query, V template)
-	{
-		this( space, query, SpatialDatabase.ALL_GROUPS, DEFAULT_MAX_RESULTS, null, DEFAULT_SHARED, template );
-	}
-	
-	public SteerSeparation(SpatialDatabase<V> space, float query, long groups, int max, V template)
-	{
-		this( space, query, groups, max, null, DEFAULT_SHARED, template );
-	}
-	
-	public SteerSeparation(SpatialDatabase<V> space, float query, long groups, int max, Filter<V, SpatialEntity<V>> filter, V template)
-	{
-		this( space, query, groups, max, filter, DEFAULT_SHARED, template );
-	}
-		
-	public SteerSeparation(SpatialDatabase<V> space, float query, long groups, int max, Filter<V, SpatialEntity<V>> filter, boolean shared, V template)
-	{
-		super(space, query, groups, max, filter, shared);
+
+    public SteerSeparation(SpatialDatabase<V> space, float query, V template)
+    {
+        this( space, query, SpatialDatabase.ALL_GROUPS, DEFAULT_MAX_RESULTS, null, DEFAULT_SHARED, template );
+    }
+
+    public SteerSeparation(SpatialDatabase<V> space, float query, long groups, int max, V template)
+    {
+        this( space, query, groups, max, null, DEFAULT_SHARED, template );
+    }
+
+    public SteerSeparation(SpatialDatabase<V> space, float query, long groups, int max, Filter<V, SpatialEntity<V>> filter, V template)
+    {
+        this( space, query, groups, max, filter, DEFAULT_SHARED, template );
+    }
+
+    public SteerSeparation(SpatialDatabase<V> space, float query, long groups, int max, Filter<V, SpatialEntity<V>> filter, boolean shared, V template)
+    {
+        super(space, query, groups, max, filter, shared);
 
         this.towards = template.create();
         this.force = template.create();
-	}
-	
-	@Override
-	public void getForce( float elapsed, SteerSubject<V> subject, V out )
-	{
-	    force = out;
-	    
-		int total = search( subject );
-		
-		if (total > 0)
-		{
-			maximize( subject, force );
-		}
-	}
+    }
 
-	@Override
-	public boolean onFoundInView( SpatialEntity<V> entity, float overlap, int index, V queryOffset, float queryRadius, int queryMax, long queryGroups )
-	{
-		towards.directi( entity.getPosition(), queryOffset );
-		towards.normalize2d();
-		
-		force.addi( towards );
-		
-		return true;
-	}
-	
-	@Override
-	public Steer<V> clone()
-	{
-		return new SteerSeparation<V>( space, query, groups, max, filter, shared, force );
-	}
-	
+    @Override
+    public void getForce( float elapsed, SteerSubject<V> subject, V out )
+    {
+        force = out;
+
+        int total = search( subject );
+
+        if (total > 0)
+        {
+            maximize( subject, force );
+        }
+    }
+
+    @Override
+    public boolean onFoundInView( SpatialEntity<V> entity, float overlap, int index, V queryOffset, float queryRadius, int queryMax, long queryGroups )
+    {
+        towards.directi( entity.getPosition(), queryOffset );
+        towards.normalize();
+
+        force.addi( towards );
+
+        return true;
+    }
+
+    @Override
+    public Steer<V> clone()
+    {
+        return new SteerSeparation<V>( space, query, groups, max, filter, shared, force );
+    }
+
 }
--- Java/src/org/magnos/steer/target/TargetSlowest.java
+++ Java/src/org/magnos/steer/target/TargetSlowest.java
@@ -12,80 +12,80 @@ import org.magnos.steer.vec.Vec;
 public class TargetSlowest<V extends Vec<V>> implements Target<V>, SearchCallback<V>
 {
 
-	public SpatialDatabase<V> space;
+    public SpatialDatabase<V> space;
     public Filter<V, SpatialEntity<V>> filter;
-	public float queryOffset;
-	public float queryRadius;
-	public boolean contains;
-	public int max;
-	public long groups;
-	
-	public float slowestVelocitySq;
-	public final V queryPosition;
-	public final V target;
-	
-	protected SteerSubject<V> subject;
+    public float queryOffset;
+    public float queryRadius;
+    public boolean contains;
+    public int max;
+    public long groups;
 
-	public TargetSlowest(SpatialDatabase<V> space, Filter<V, SpatialEntity<V>> filter, float queryOffset, float queryRadius, boolean contains, int max, long groups, V template)
-	{
-		this.space = space;
-		this.filter = filter;
-		this.queryOffset = queryOffset;
-		this.queryRadius = queryRadius;
-		this.contains = contains;
-		this.max = max;
-		this.groups = groups;
-		this.queryPosition = template.create();
-		this.target = template.create();
-	}
-	
-	@Override
-	public V getTarget( SteerSubject<V> s )
-	{
-	    subject = s;
-		slowestVelocitySq = Float.MAX_VALUE;
-		
-		queryPosition.set( subject.getPosition() );
-		queryPosition.addsi( subject.getDirection(), queryOffset );
-		
-		int found = 0;
-		
-		if ( contains )
-		{
-			found = space.contains( queryPosition, queryRadius, max, groups, this );
-		}
-		else
-		{
-			found = space.intersects( queryPosition, queryRadius, max, groups, this );
-		}
-		
-		if ( found == 0 )
-		{
-			return null;
-		}
-		
-		return target;
-	}
+    public float slowestVelocitySq;
+    public final V queryPosition;
+    public final V target;
 
-	@Override
-	public boolean onFound( SpatialEntity<V> entity, float overlap, int index, V queryOffset, float queryRadius, int queryMax, long queryGroups )
-	{
-		boolean applicable = (entity instanceof SteerSubject) && (filter == null || filter.isValid( subject, entity ));
+    protected SteerSubject<V> subject;
 
-		if ( applicable )
-		{
-			SteerSubject<V> subject = (SteerSubject<V>)entity;
-			
-			float vsq = subject.getVelocity().lengthSq2d();
-			
-			if ( vsq < slowestVelocitySq )
-			{
-				slowestVelocitySq = vsq;
-				target.set( subject.getPosition() );
-			}
-		}
-		
-		return applicable;
-	}
+    public TargetSlowest(SpatialDatabase<V> space, Filter<V, SpatialEntity<V>> filter, float queryOffset, float queryRadius, boolean contains, int max, long groups, V template)
+    {
+        this.space = space;
+        this.filter = filter;
+        this.queryOffset = queryOffset;
+        this.queryRadius = queryRadius;
+        this.contains = contains;
+        this.max = max;
+        this.groups = groups;
+        this.queryPosition = template.create();
+        this.target = template.create();
+    }
+
+    @Override
+    public V getTarget( SteerSubject<V> s )
+    {
+        subject = s;
+        slowestVelocitySq = Float.MAX_VALUE;
+
+        queryPosition.set( subject.getPosition() );
+        queryPosition.addsi( subject.getDirection(), queryOffset );
+
+        int found = 0;
+
+        if ( contains )
+        {
+            found = space.contains( queryPosition, queryRadius, max, groups, this );
+        }
+        else
+        {
+            found = space.intersects( queryPosition, queryRadius, max, groups, this );
+        }
+
+        if ( found == 0 )
+        {
+            return null;
+        }
+
+        return target;
+    }
+
+    @Override
+    public boolean onFound( SpatialEntity<V> entity, float overlap, int index, V queryOffset, float queryRadius, int queryMax, long queryGroups )
+    {
+        boolean applicable = (entity instanceof SteerSubject) && (filter == null || filter.isValid( subject, entity ));
+
+        if ( applicable )
+        {
+            SteerSubject<V> subject = (SteerSubject<V>)entity;
+
+            float vsq = subject.getVelocity().lengthSq();
+
+            if ( vsq < slowestVelocitySq )
+            {
+                slowestVelocitySq = vsq;
+                target.set( subject.getPosition() );
+            }
+        }
+
+        return applicable;
+    }
 
 }
